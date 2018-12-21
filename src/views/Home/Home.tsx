123be4eb566320a47802c5284fe8cc5e2a51e643
James Trent <jamestrent@jamess-imac-pro.uls.edu>
2018-12-21 16:08:21 -0500

    homecss test

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import './Home.css'
+import './home2.css'
 import Helmet from 'react-helmet'
 import { connect } from 'react-redux'
 import { ReduxState } from '../../types/redux'
